bc17adaabd1e49a203fcbc824acce78ecd9d4c25
suracI-invert <koolsha123@gmail.com>
2026-06-16 10:15:43 +0700

    cache-bump

--- cache/src/sunbot/sdk/cache/redis/__init__.py
+++ cache/src/sunbot/sdk/cache/redis/__init__.py
@@ -1,1 +1,13 @@
-class Redis: ...
+from redis import Redis as RedisCache
+from sunbot.core.cache import CacheRead
+
+
+class Redis(CacheRead):
+    def __init__(self, client: RedisCache):
+        self._client = client
+
+    async def get(self, key: str) -> bytes:
+        data = self._client.get(key)
+        if not data:
+            raise KeyError
+        return data.encode("utf-8") if isinstance(data, str) else data
