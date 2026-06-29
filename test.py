87453111d128c0368636dc3b5f8886c869bf3907
suracI-invert <koolsha123@gmail.com>
2026-06-29 13:48:18 +0700

    fix workspace sources

--- test.py
+++
@@ -1,6 +0,0 @@
-from sunbot.core.cache import CacheRead
-from sunbot.sdk.cache.mem import InMemoryCache
-from sunbot.sdk.cache.redis import Redis
-
-from sunbot.core.llm import StructuredOutputGeneration, TextGeneration
-from sunbot.sdk.llm.openai import OpenAI
